ab5738667cfe064d4eb3177b2217b72aa247f968
Shawn Wells <shawn@redhat.com>
2018-01-04 23:44:30 -0500

    update port to env variable

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ function loadDatabase (cb) {
  */
 program
   .command('server')
-  .option('-p --port <port>', 'Server port', 8080, parseInt)
+  .option('-p --port <port>', 'Server port', process.env.PORT, parseInt)
   .description('Run preview server')
   .action(function (options) {
     var app = require('./app')
