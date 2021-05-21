2989414103b5461ddd0fa32b74040f555e4a2a6c
Joe English <jenglish@flightlab.com>
2021-05-21 15:36:20 -0700

    test-server.js: use process.env.PORT if present.

--- examples/test/test-server.js
+++ examples/test/test-server.js
@@ -4,11 +4,12 @@ var app = require('../../app')
   , mock = require('../..//mock')
   , http = require('http')
   , server = http.createServer(app)
+  , port = process.env.PORT || 3000
   ;
 
 opencontrol.load(mock.config, function (err, db) {
     if (err) { return err; }
     app.initialize(mock.config, db);
-    server.listen(3000);
+    server.listen(port);
 });
 
